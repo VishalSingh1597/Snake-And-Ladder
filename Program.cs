106e966a9bb4bcc9ccf8f402aaf01eda93a8b1ed
Vishal Singh <VishalSingh1597>
2021-04-15 22:07:55 +0530

    Added a Code

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int position = 0;
+            Console.WriteLine("Start position of player: " + position);
+            Console.ReadLine();
+        }
+    }
+}
